9f963ffabc4ab0797fb086c7d39af8608ccf180e
Mohammed Mustafa <88572251+momustafam@users.noreply.github.com>
2024-05-09 21:18:37 +0300

    Delete 0-uniq_users.sql

--- 0-uniq_users.sql
+++
@@ -1,7 +0,0 @@
--- Create a table `users`
--- With these attributes `id`, `email`, `name`
-CREATE TABLE IF NOT EXISTS users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
-	);
