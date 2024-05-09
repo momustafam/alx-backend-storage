a87166945937e676a082d7fd62f649504cf085df
Mohammed Mustafa <mohammedmustafa112025@gmail.com>
2024-05-09 18:57:50 +0000

    auto pushing

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- Create a table `users`
 -- with 3 attributes `id`, `email`, `name`, `country`
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
