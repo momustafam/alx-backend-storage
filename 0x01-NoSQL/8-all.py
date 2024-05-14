74ef5bf14359efcd2321eea47da7e6f8819eb171
Mohammed Mustafa <mohammedmustafa112025@gmail.com>
2024-05-14 15:28:07 -0700

    auto pushing

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,13 +1,9 @@
-#!/user/bin/env python3
-'''Simple module since it has a simple function `list_all(collection)`.'''
+#!/usr/bin/env python3
+"""Module that contains list_all"""
 
 
 def list_all(mongo_collection):
-    '''Lists all documents in a mongo collection.
+    """lists all documents in a collection"""
+    docs = mongo_collection.find()
 
-    Parameters:
-        - model_collection: the collection that contains the documents
-
-    Return: List all documents of a the given collection
-    '''
-    return mongo_collection.find()
+    return docs
