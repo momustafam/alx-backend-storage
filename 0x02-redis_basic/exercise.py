6bfc51502440a20bf8284ef4e37b6c4e362a2bc3
Mohammed Mustafa <mohammedmustafa112025>
2024-05-15 23:44:12 +0000

    auto pushing

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,26 +1,81 @@
 #!/usr/bin/env python3
-'''Simple module since it has a simple class.'''
+"""Module that creates Cache class."""
 import redis
 import uuid
-from typing import Union, Collable, Optional
+import asyncio
+from functools import wraps
+from typing import Union, Callable, Any, Optional
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+        A decorator that count how many times
+        methods of the Cache class are called
+    """
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        """Invokes the given method after incrementing its call counter."""
+        if isinstance(self._redis, redis.Redis):
+            self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return invoker
+
+
+def call_history(method: Callable) -> Callable:
+    """Tracks the call details of a method in a Cache class"""
+    @wraps(method)
+    def invoker(self, *args, **kwargs) -> Any:
+        """Returns the method's output after storing its inputs and output"""
+        in_key = '{}:inputs'.format(method.__qualname__)
+        out_key = '{}:outputs'.format(method.__qualname__)
+        self._redis.rpush(in_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(out_key, output)
+        return output
+    return invoker
+
+
+def replay(fn: Callable) -> None:
+    '''Displays the call history of a Cache class' method.
+    '''
+    if fn is None or not hasattr(fn, '__self__'):
+        return
+    redis_store = getattr(fn.__self__, '_redis', None)
+    if not isinstance(redis_store, redis.Redis):
+        return
+    fxn_name = fn.__qualname__
+    in_key = '{}:inputs'.format(fxn_name)
+    out_key = '{}:outputs'.format(fxn_name)
+    fxn_call_count = 0
+    if redis_store.exists(fxn_name) != 0:
+        fxn_call_count = int(redis_store.get(fxn_name))
+    print('{} was called {} times:'.format(fxn_name, fxn_call_count))
+    fxn_inputs = redis_store.lrange(in_key, 0, -1)
+    fxn_outputs = redis_store.lrange(out_key, 0, -1)
+    for fxn_input, fxn_output in zip(fxn_inputs, fxn_outputs):
+        print('{}(*{}) -> {}'.format(
+            fxn_name,
+            fxn_input.decode("utf-8"),
+            fxn_output,
+        ))
 
 
 class Cache:
-    '''Simple class that trying to cach some clients data.'''
-
-    def __init__(self):
-        '''Instantiate an object of Cache class.'''
+    def __init__(self) -> None:
+        """Sets Up a connection to Redis"""
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        '''Store the input data in Redis.'''
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        """Stores a value in a Redis data storage and returns the ke"""
+        rand_key = str(uuid.uuid4())
+        self._redis.set(rand_key, data)
+        return rand_key
 
-    def get(key: str, fn: Optional[Collable] = None) -> Union[
-            str, bytes, int, float]:
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """Gets and value from Redis data storage assiociated with key"""
         value = self._redis.get(key)
         return fn(value) if fn else value
