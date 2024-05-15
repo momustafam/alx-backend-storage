81b1c603cba2a98947536fb4f1eeb20a3a26a3ab
Mohammed Mustafa <mohammedmustafa112025>
2024-05-15 23:53:35 +0000

    auto pushing

---
+++ 0x02-redis_basic/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import redis
+
+Cache = __import__('exercise').Cache
+cache = Cache()
+cache.store("foo")
+cache.store("bar")
+cache.store(42)
+replay(cache.store)
